9fcbe5204697546ec2b5268b637b0012a582eb17
Goldstack Project Builder <goldstack@pureleap.com>
2022-10-08 21:04:04 +0000

    Update boilerplate

---
+++ packages/server-side-rendering-1/src/render.ts
@@ -0,0 +1,39 @@
+import { getDeployment, PartialRenderPageProps } from '@goldstack/template-ssr';
+
+import {
+  renderPage as ssrRenderPage,
+  hydrate as ssrHydrate,
+} from '@goldstack/template-ssr';
+import { APIGatewayProxyResultV2 } from 'aws-lambda';
+
+import Wrapped from './_app';
+
+import staticFileMapperStore from './state/staticFiles.json';
+
+import goldstackJson from './../goldstack.json';
+
+import renderDocument from './_document';
+
+import buildConfig from './build';
+
+export async function renderPage<P>(
+  props: PartialRenderPageProps<P>
+): Promise<APIGatewayProxyResultV2> {
+  const deployment = getDeployment(goldstackJson);
+  return ssrRenderPage({
+    staticFileMapperStore,
+    renderDocument,
+    deployment,
+    buildConfig: () => {
+      return buildConfig();
+    },
+    ...props,
+    component: Wrapped({
+      Component: props.component,
+    }),
+  });
+}
+
+export function hydrate(c: React.FunctionComponent<any>): void {
+  ssrHydrate(Wrapped({ Component: c }));
+}
---
+++ packages/server-side-rendering-1/src/routes/$index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { SSRHandler } from '@goldstack/template-ssr';
+
+import { renderPage, hydrate } from './../render';
+import Panel from './../components/Panel';
+import styles from './$index.module.css';
+
+const Index = (props: { message: string }): JSX.Element => {
+  const [clicked, setClicked] = useState(false);
+  return (
+    <>
+      <div
+        onClick={() => {
+          alert('hi');
+          setClicked(true);
+          throw new Error('Havent seen this');
+        }}
+        className={`${styles.message}`}
+      >
+        {props.message}
+      </div>
+      {clicked && <Panel />}
+    </>
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const handler: SSRHandler = async (event, context) => {
+  return renderPage({
+    component: Index,
+    appendToHead: '<title>SSR Template</title>',
+    properties: {
+      message: 'Hi there',
+    },
+    entryPoint: __filename,
+    event,
+  });
+};
+
+hydrate(Index);
+
+export default Index;
