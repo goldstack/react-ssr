da58ad718ffbb359d495b32fffb91f4bd3d427e9
Goldstack Project Builder <1448524+mxro@users.noreply.github.com>
2024-12-28 01:39:05 +0000

    Automated update of generated boilerplate by goldstack.party

--- .pnp.cjs
+++ .pnp.cjs
@@ -33,7 +33,7 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./",\
         "packageDependencies": [\
           ["@goldstack/nodemonx", "npm:0.4.21"],\
-          ["@goldstack/utils-typescript-references", "npm:0.3.12"],\
+          ["@goldstack/utils-typescript-references", "npm:0.3.13"],\
           ["@types/jest", "npm:29.5.14"],\
           ["@types/node", "npm:22.10.2"],\
           ["@typescript-eslint/eslint-plugin", "virtual:ea1d9e7049b95ed362294c9df397aab9c8003d7203ea20a644a1562064d0f435c693872484b439325fdea8e2b85d16d249f14b9d624705186342a0eb450de825#npm:5.42.1"],\
@@ -3635,10 +3635,11 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@goldstack/utils-typescript-references", [\
-      ["npm:0.3.12", {\
-        "packageLocation": "./.yarn/cache/@goldstack-utils-typescript-references-npm-0.3.12-e4fc97f991-e81a7fc2f2.zip/node_modules/@goldstack/utils-typescript-references/",\
+      ["npm:0.3.13", {\
+        "packageLocation": "./.yarn/cache/@goldstack-utils-typescript-references-npm-0.3.13-490b1e9794-74dfa118db.zip/node_modules/@goldstack/utils-typescript-references/",\
         "packageDependencies": [\
-          ["@goldstack/utils-typescript-references", "npm:0.3.12"],\
+          ["@goldstack/utils-typescript-references", "npm:0.3.13"],\
+          ["@goldstack/utils-log", "npm:0.3.18"],\
           ["source-map-support", "npm:0.5.21"],\
           ["yargs", "npm:17.5.1"]\
         ],\
@@ -14226,7 +14227,7 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["project-server-side-rendering", "workspace:."],\
           ["@goldstack/nodemonx", "npm:0.4.21"],\
-          ["@goldstack/utils-typescript-references", "npm:0.3.12"],\
+          ["@goldstack/utils-typescript-references", "npm:0.3.13"],\
           ["@types/jest", "npm:29.5.14"],\
           ["@types/node", "npm:22.10.2"],\
           ["@typescript-eslint/eslint-plugin", "virtual:ea1d9e7049b95ed362294c9df397aab9c8003d7203ea20a644a1562064d0f435c693872484b439325fdea8e2b85d16d249f14b9d624705186342a0eb450de825#npm:5.42.1"],\
